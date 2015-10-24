adbad0f6c7d12cc99b1c43ffa8448f4819cd950a
Rollie Ma <rollie.ma@gmail.com>
2015-10-23 19:08:14 -0700

    Index template params and templates

--- routes/index.go
+++ routes/index.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/waltzofpearls/tetris.go/libs"
@@ -20,5 +21,15 @@ func (sr *Index) AttachHandlers() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) error {
-	return libs.ExecuteTemplate(w, "index", 200, nil)
+	return libs.ExecuteTemplate(w, "index", 200, map[string]interface{}{
+		"Title": "Rollie Ma - Polyglot Developer from Vancouver, BC",
+		"Description": strings.Join([]string{
+			"Hi, I'm Rollie Ma. A Linux lover and LEGO bricks enthusiast.",
+			"A polyglot developer obsessed with PHP, Python and JavaScript.",
+			"A receptive learner captivated by mobile development, NoSQL and data mining.",
+			"An amateur explorer interested in information aggregation and artificial intelligence fields.",
+		}, " "),
+		"Url":   "http://rolli3.net",
+		"Image": "http://rolli3.net/images/logos/logo-120x120.png",
+	})
 }
