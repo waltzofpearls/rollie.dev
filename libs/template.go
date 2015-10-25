653013b85b965aed68ad58dde3fb369dc43831a1
Rollie Ma <rollie.ma@gmail.com>
2015-10-25 14:03:30 -0700

    Added custom template function "moment"

--- libs/template.go
+++ libs/template.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"net/http"
 	"text/template"
+	"time"
 )
 
 var tpls = map[string]*template.Template{
@@ -11,8 +12,16 @@ var tpls = map[string]*template.Template{
 	"index": newTemplate("views/layout.html", "views/index.html"),
 }
 
+var funcs = template.FuncMap{
+	"moment": moment,
+}
+
+func moment(fmt string) string {
+	return time.Now().Format(fmt)
+}
+
 func newTemplate(files ...string) *template.Template {
-	return template.Must(template.New("*").ParseFiles(files...))
+	return template.Must(template.New("*").Funcs(funcs).ParseFiles(files...))
 }
 
 func ExecuteTemplate(w http.ResponseWriter, name string, status int, data interface{}) error {
