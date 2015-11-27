fe10cf9d21ee8985680915af0ddbb7e641037797
Rollie Ma <rollie.ma@gmail.com>
2015-11-26 23:58:40 -0800

    Added a new test case

---
+++ libs/app_test.go
@@ -0,0 +1,16 @@
+package libs_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/waltzofpearls/tetris-go/libs"
+)
+
+func TestCreateApp(t *testing.T) {
+	config := libs.NewConfig()
+	require.NotNil(t, config)
+	app := libs.NewApp(config)
+	assert.NotNil(t, app)
+}
